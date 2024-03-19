802180225a0d21ff68993ef573815da7b088da97
dubiao <biaoduc@isoftstone.com>
2024-03-19 16:18:24 +0800

    Site updated: 2024-03-19 16:18:23

---
+++ js/myscript.js
@@ -0,0 +1,252 @@
+
+const collectList = [
+    {
+        classify: "小书推荐",
+        list: [
+            {
+                siteName: "ECMAScript 6 入门",
+                siteUrl: "https://es6.ruanyifeng.com",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/es6.png"
+            },
+            {
+                id: 2,
+                siteName: "koa进阶笔记",
+                siteUrl: "https://chenshenhai.github.io/koa2-note/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/koa.png"
+            },
+            {
+                id: 3,
+                siteName: "Vue.js 技术揭秘",
+                siteUrl: "https://ustbhuangyi.github.io/vue-analysis/v2/prepare/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/vue.png"
+            },
+            {
+                id: 4,
+                siteName: "木易杨前端进阶",
+                siteUrl: "https://muyiy.cn/blog/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/muyiy.png"
+            },
+            {
+                id: 5,
+                siteName: "Nginx 入门教程 ",
+                siteUrl: "https://xuexb.github.io/learn-nginx/example/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/ngnix.png"
+            },
+            {
+                id: 6,
+                siteName: "labuladong的算法小抄",
+                siteUrl: "https://labuladong.github.io/algo/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/labuladong.png"
+            },
+            {
+                id: 7,
+                siteName: "认识 React",
+                siteUrl: "https://react.nodejs.cn/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/react.png"
+            },
+            {
+                id: 8,
+                siteName: "深入浅出 Webpack",
+                siteUrl: "https://webpack.wuhaolin.cn/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/webpack.png"
+            },
+            {
+                id: 9,
+                siteName: "前端面试进阶",
+                siteUrl: "https://interview.poetries.top/",
+                siteText: "1",
+                classify: "小书推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/interview.png"
+            }
+        ]
+    },
+    {
+        classify: "技术社区",
+        list: [
+            {
+                siteName: "StackOverflow",
+                siteUrl: "https://stackoverflow.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/stackoverflow.png"
+            },
+            {
+                siteName: "GitHub",
+                siteUrl: "https://github.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/github.png"
+            },
+            {
+                siteName: "掘金",
+                siteUrl: "https://juejin.im/ ",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/juejin.png"
+            },
+            {
+                siteName: "Segmentfault",
+                siteUrl: "https://segmentfault.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/segmentfault.png"
+            },
+            {
+                siteName: "V2EX",
+                siteUrl: "https://www.v2ex.com/ ",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/v2ex.png"
+            },
+            {
+                siteName: "慕课网",
+                siteUrl: "https://www.imooc.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/imooc.png"
+            },
+            {
+                siteName: "W3cschool",
+                siteUrl: "https://www.w3school.com.cn/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/w3c.png"
+            },
+            {
+                siteName: "廖雪峰官方网站",
+                siteUrl: "https://www.liaoxuefeng.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/liaoxuefeng.png"
+            },
+        ]
+    },
+    {
+        classify: "工具推荐",
+        list: [
+            {
+                siteName: "jsdeliver",
+                siteUrl: "https://www.jsdelivr.com/",
+                siteText: "1",
+                classify: "技术社区",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/jsdeliver.png"
+            },
+            {
+                siteName: "tinypng-图片压缩神器",
+                siteUrl: "https://tinify.cn/",
+                siteText: "1",
+                classify: "工具推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/tinify.png"
+            },
+            {
+                siteName: "Regexr-正则测试",
+                siteUrl: "https://regexr.com/",
+                siteText: "1",
+                classify: "工具推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/regexr.png"
+            },
+            {
+                siteName: "Processon -流程图",
+                siteUrl: "https://www.processon.com/",
+                siteText: "1",
+                classify: "工具推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/process.png"
+            },
+            {
+                siteName: "文件格式转换",
+                siteUrl: "https://cn.onlineconvert.com/",
+                siteText: "1",
+                classify: "工具推荐",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/onlineconvert.png"
+            },
+            {
+                siteName: "Markdown 组件应用(基于 vue)",
+                siteUrl: "http://ckang1229.gitee.io/vue-markdown-editor/zh/",
+                siteText: "Markdown 组件应用",
+                classify: "工具推荐",
+                sitePic: "/img/avatar.png"
+            }
+        ]
+    },
+    {
+        classify: "设计与图片",
+        list: [
+            {
+                siteName: "花瓣网",
+                siteUrl: "https://huaban.com/",
+                siteText: "1",
+                classify: "设计与图片",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/huaban.png"
+            },
+            {
+                siteName: "Pixabay",
+                siteUrl: "https://pixabay.com/zh/",
+                siteText: "1",
+                classify: "设计与图片",
+                sitePic: "https://cdn.jsdelivr.net/gh/xiaolingdmn/picfiles/pixabay.png"
+            },
+            {
+                siteName: "Pexels",
+                siteUrl: "https://www.pexels.com/",
+                siteText: "1",
+                classify: "设计与图片",
+                sitePic: "/img/avatar.png"
+            },
+        ]
+    }
+]
+
+const container = document.getElementById('list-container');
+let allHtml = ''
+collectList.forEach(item => {
+    let strHtml = '';
+    strHtml += `    
+   <div
+       class="home_content mt-20"
+   >
+       <div class="home_card_classify">
+      ${item.classify}
+       </div>
+       <ul class="d-flex mt-20 mb-20 home_card_list">
+     
+   
+`
+    item.list.forEach(i => {
+        strHtml += `<li  class="mb-20 home_site_item">
+        <a
+        class="d-flex mr-20"
+        href='${i.siteUrl}'
+        title='${i.siteText}'
+        target="_blank"
+      >
+        <div class="pic">
+            <img class="mr-10 d-inline-block" src='${i.sitePic}'  />
+        </div>
+        <span> ${i.siteName}
+        </span>
+      </a>
+</li > `
+    });
+    strHtml += `
+    </ul >
+    </div > `
+    allHtml += strHtml
+});
+
+container.innerHTML = allHtml
